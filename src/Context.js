220e0f576a7add1a00d84a88d5767abad19d9d6b
enpleinair23 <enpleinair@mail.com>
2024-01-17 21:30:57 +0000

    aaa

---
+++ src/Context.js
@@ -0,0 +1,140 @@
+import { createContext, useCallback, useReducer } from "react";
+
+// Create Context
+const TokyoContext = createContext();
+
+// Type
+const type = {
+  NAV: "NAV",
+  ANIMATION: "ANIMATION",
+  MODAL: "MODAL",
+  SERVICEMODAL: "SERVICEMODAL",
+  PORTFOLIODETAILSMODAL: "PORTFOLIODETAILSMODAL",
+};
+const {
+  NAV,
+  ANIMATION,
+  MODAL,
+  SERVICEMODAL,
+  PORTFOLIODETAILSMODAL,
+} = type;
+
+// Initial Value
+const initialState = {
+  nav: "home",
+  animation: "fadeInLeft",
+  modal: false,
+  serviceModal: null,
+  portfolioDetailsModal: null,
+  menus: [
+    { id: 1, name: "Home", href: "home" },
+    { id: 2, name: "portfolio", href: "portfolio" },
+    { id: 3, name: "service", href: "service" },
+    { id: 4, name: "about", href: "about" },
+    { id: 5, name: "contact", href: "contact" },
+  ],
+};
+
+// Reducer
+const reducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case NAV:
+      return {
+        ...state,
+        nav: payload,
+      };
+    case ANIMATION:
+      return {
+        ...state,
+        animation: payload,
+      };
+    case MODAL:
+      return {
+        ...state,
+        modal: payload,
+      };
+    case SERVICEMODAL:
+      return {
+        ...state,
+        serviceModal: payload,
+      };
+    case PORTFOLIODETAILSMODAL:
+      return {
+        ...state,
+        portfolioDetailsModal: payload,
+      };
+    default:
+      return state;
+  }
+};
+
+// Watson State
+const TokyoState = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const navChange = useCallback((value) => {
+    dispatch({
+      type: NAV,
+      payload: value,
+    });
+  }, []);
+
+  const animationChnage = useCallback((value) => {
+    dispatch({
+      type: ANIMATION,
+      payload: value,
+    });
+  }, []);
+
+  const modalToggle = useCallback((value) => {
+    dispatch({
+      type: MODAL,
+      payload: value,
+    });
+  }, []);
+
+  const setServiceModal = useCallback((value) => {
+    dispatch({
+      type: SERVICEMODAL,
+      payload: value,
+    });
+  }, []);
+  const setPortfolioDetailsModal = useCallback((value) => {
+    dispatch({
+      type: PORTFOLIODETAILSMODAL,
+      payload: value,
+    });
+  }, []);
+
+  const {
+    nav,
+    animation,
+    modal,
+    serviceModal,
+    portfolioDetailsModal,
+    menus,
+  } = state;
+  return (
+    <TokyoContext.Provider
+      value={{
+        menus,
+        nav,
+        navChange,
+        animation,
+        animationChnage,
+        modal,
+        modalToggle,
+        serviceModal,
+        setServiceModal,
+        portfolioDetailsModal,
+        setPortfolioDetailsModal,
+      }}
+    >
+      {children}
+    </TokyoContext.Provider>
+  );
+};
+
+export default TokyoState;
+export { TokyoContext };
