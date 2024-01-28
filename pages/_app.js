1eca9c7ad7ee3ef68f8984788a8a183a5e0fe078
enpleinair23 <enpleinair@mail.com>
2024-01-28 16:05:05 +0000

    eee

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import TokyoState from "@/src/Context";
 import PreLoader from "@/src/layout/PreLoader";
 import "@/styles/globals.css";
+import Head from "next/head"
 
 export default function App({ Component, pageProps }) {
   return (
     <TokyoState>
+    <Head>
+  <link rel="icon" href="/icon.ico" />
+    </Head>
       <PreLoader />
       <Component {...pageProps} />
     </TokyoState>
